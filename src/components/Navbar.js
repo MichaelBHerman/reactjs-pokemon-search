0a09f4a5a4c585c4cbcf79b872f7e8aed890d006
MichaelBHerman <michaelbherman@gmail.com>
2021-12-20 21:52:36 -0700

    trainer card added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,14 @@ import { NavLink } from 'react-router-dom';
 import jigglypuff from "./images/jigglypuff.gif";
 import HomePage from '../pages/home';
 import audrey from './images/audrey2.png';
+import trainer from './images/trainer1.png';
 
 function Navbar() {
     return (
         <div className="Navbar">
-            
+            <nav>
+                {/* <img src={trainer} className='Navlogo2'/> */}
+            </nav>
             {/* <img src={jigglypuff} className='Jigglypuff'/> */}
             <img src={audrey} className='Navlogo1' />
             <img src={navlogo} className='Navlogo'/>
