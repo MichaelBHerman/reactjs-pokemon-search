0ea49b129781e81805052f1ab95b5ae3e2b51ef7
MichaelBHerman <michaelbherman@gmail.com>
2021-12-20 20:16:52 -0700

    Added graphic to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,14 @@ import navlogo from './images/pokedex.png';
 import { NavLink } from 'react-router-dom';
 import jigglypuff from "./images/jigglypuff.gif";
 import HomePage from '../pages/home';
-
+import audrey from './images/audrey2.png';
 
 function Navbar() {
     return (
         <div className="Navbar">
             
             {/* <img src={jigglypuff} className='Jigglypuff'/> */}
+            <img src={audrey} className='Navlogo1' />
             <img src={navlogo} className='Navlogo'/>
             <HomePage />
             {/* <nav>
